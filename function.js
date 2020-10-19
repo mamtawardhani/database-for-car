f98766395f1e20a58b31f5b31eda1081475fb861
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2020-10-19 23:33:51 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,36 @@
+function enterplayer(){
+
+    hello=createElement('h1')
+    hello.position(displayWidth/2-70,displayHeight/4)
+    var name=input.value()
+  
+   hello.html("Welcome " + name + "! Wait for the other players to join!");
+
+    button.hide()
+    input.hide()
+    reset.hide()
+    //title.hide()
+    //hello.hide()
+    pc=pc+1;
+
+    db.ref('/').update({
+        playercount: pc
+    })
+
+    db.ref("players/player"+pc).set({
+        y:590,
+        index:pc
+    })
+
+    currentindex=pc
+   
+}
+
+function resetD(){
+    db.ref('/').update({
+        gamestate:0,
+        playercount:0
+    })
+    db.ref('players').remove()
+}
+
